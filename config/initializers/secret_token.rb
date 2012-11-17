6c7b8ccf302af505169534fe1838354dae261dd4
tatsuo.sakurai <tatsuo.sakurai@gmail.com>
2012-11-17 16:23:25 +0900

    rails new bop

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bop::Application.config.secret_token = '1f1bd0df614567030c5cbc8fe3f004008a3b59f9a8c8b34c770cc1dbc9a3db0816c117e22619dd54873ad3bfc9d3319c1d1ada800c597a4136af4540ecc13868'
