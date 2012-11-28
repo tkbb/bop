474bb36523a9ce98c9123901b5b53af1c25afea2
vestige <vestige_sync@me.com>
2012-11-28 21:08:17 +0900

    add column Name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121117105931) do
+ActiveRecord::Schema.define(:version => 20121128120622) do
 
   create_table "plans", :force => true do |t|
     t.text     "url"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(:version => 20121117105931) do
     t.text     "gyms"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "name"
   end
 
 end
