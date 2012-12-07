bceaede7d3a019ee42f1000b6bcc5e656c0ea877
vestige <vestige_sync@me.com>
2012-12-07 22:47:41 +0900

    modify seed for plan data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,12 @@
-Plan.create(:name => "2012.12 in tkbb", :url => "http://localhost/123/", :month => 201212, :days => "{date: 1, count: 3}, {date: 5, count: 2}", :comments => "{name: 'vestige', says: 'one'}, {name:, 't2o', says: 'two'}", :gyms => "{name: 'ogikubo', count: 4}, {name: 'akihabara', count: 2}")
-Plan.create(:name => "2012.11 in tkbb", :url => "http://localhost/125/", :month => 201211, :days => "{date: 10, count: 3}, {date: 25, count: 1}", :comments => "{name: 'vestige', says: 'one'}, {name:, 't2o', says: 'two'}", :gyms => "{name: 'ogikubo', count: 4}, {name: 'akihabara', count: 2}")
+Plan.create(name: "2012.12 in tkbb",
+	        url: "http://localhost/123/",
+	        month: 201212,
+	        days: "{date: 1, count: 3}, {date: 5, count: 2}",
+	        comments: "{name: 'vestige', says: 'one'}, {name:, 't2o', says: 'two'}",
+	        gyms: "{name: 'ogikubo', count: 4}, {name: 'akihabara', count: 2}")
+Plan.create(name: "2012.11 in tkbb",
+			url: "http://localhost/125/", 
+			month: 201211,
+			days: "{date: 10, count: 3}, {date: 25, count: 1}",
+			comments: "{name: 'vestige', says: 'one'}, {name:, 't2o', says: 'two'}",
+			gyms: "{name: 'ogikubo', count: 4}, {name: 'akihabara', count: 2}")
