432307d9b96159c47d2eb83726d093168739a39d
tatsuo.sakurai <tatsuo.sakurai@gmail.com>
2012-12-08 03:00:53 +0900

    Use factorygirl

--- spec/factories/plans.rb
+++ spec/factories/plans.rb
@@ -2,10 +2,12 @@
 
 FactoryGirl.define do
   factory :plan do
-    url ""
-    month ""
-    days ""
-    comments ""
-    gyms "MyText"
+    sequence(:name) {|n| "tkbb plan #{n}" }
+    #TODO もっとランダムなもの
+    sequence(:url) {|n| "http://localhost/#{n}/" }
+    month { "#{Time.now.year}#{format("%02d", rand(12)+1)}" }
+    days "{date: 10, count: 3}, {date: 25, count: 1}"
+    comments "{name: 'vestige', says: 'one'}, {name:, 't2o', says: 'two'}"
+    gyms "{name: 'ogikubo', count: 4}, {name: 'akihabara', count: 2}"
   end
 end
