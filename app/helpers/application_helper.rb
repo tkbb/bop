6dc3f43f4350fec77a5ba37246d6e3c8c95fa13c
tatsuo.sakurai <tatsuo.sakurai@gmail.com>
2013-03-31 02:41:38 +0900

    Move days to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def days_of_a_month
+    days_of_the_week = %w(月 火 水 木 金 土 日)
+    days = []
+    (DateTime.now.beginning_of_month..DateTime.now.end_of_month).map do |datetime|
+      days << datetime.strftime("%Y/%m/%d(#{days_of_the_week[datetime.wday]})")
+    end
+    days
+  end
+
 end
