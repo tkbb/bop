d3b151b02fdcc0d27125b7a78bf058dfb1b4a480
tatsuo.sakurai <tatsuo.sakurai@gmail.com>
2013-03-31 03:58:29 +0900

    Refactor duplicatited DateTime.now

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,8 @@ module ApplicationHelper
   def days_of_a_month
     days_of_the_week = I18n.t('date.abbr_day_names')
     days = []
-    (DateTime.now.beginning_of_month..DateTime.now.end_of_month).map do |datetime|
+    now = DateTime.now
+    (now.beginning_of_month..now.end_of_month).map do |datetime|
       days << datetime.strftime("%Y/%m/%d(#{days_of_the_week[datetime.wday]})")
     end
     days
