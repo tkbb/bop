e8b27da29e76c4dc72ce9a7b1831b9a6d08355a2
tatsuo.sakurai <tatsuo.sakurai@gmail.com>
2014-05-18 16:00:32 +0900

    Fix attr_accessible for Rails4

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,5 +1,25 @@
 class PlansController < ApplicationController
   def index
-  	@plans = Plan.all
+    @plans = Plan.all
+  end
+
+  def new
+    @plan = Plan.new
+  end
+
+  def create
+    @plan = Plan.new(plan_params)
+
+    if @paln.save
+      redirect_to @plan, notice: 'Plan was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def plan_params
+    params.require(:plan).permit(:comments, :days, :gyms, :month, :url, :name)
   end
 end
